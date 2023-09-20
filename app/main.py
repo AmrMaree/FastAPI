69fdd64e775f11441742aab318a5392bbbc57d40
AmrMaree <113312326+AmrMaree@users.noreply.github.com>
2023-09-21 02:25:50 +0300

    checking the bug is fixed

--- app/main.py
+++ app/main.py
@@ -4,10 +4,9 @@ from .database import engine
 from .routers import user, post, auth, vote
 from .config import settings
 
-#models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
 
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {
     "title": "my favourite food", "content": "i like pizza", "id": 2}]
